6db4b52795faddef4a95dfd4d5a4d3be5919f87f
dinhngocnam <namdnfx17199@funix.edu.vn>
2022-12-31 20:33:57 +0700

    auth

--- middleware/is-Auth.js
+++ middleware/is-Auth.js
@@ -1,10 +1,10 @@
-exports.AuthLogin = (req, res, next) => {
+module.exports.AuthLogin = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         res.status(400).json({ message: 'You are not login!' })
     }
     next();
 }
-exports.AuthAdminLogin = (req, res, next) => {
+module.exports.AuthAdminLogin = (req, res, next) => {
     if (!req.session.isAdminLoggedIn) {
         res.status(400).json({ message: 'You are not login!' })
     }
