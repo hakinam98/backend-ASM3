39018c41425546d4142d003ae16ea650f0b4986b
dinhngocnam <namdnfx17199@funix.edu.vn>
2023-01-01 15:58:30 +0700

    edit auth 2

--- middleware/authclient.js
+++ middleware/authclient.js
@@ -1,6 +1,8 @@
-exports.AuthLogin = (req, res, next) => {
+const AuthLogin = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         res.status(400).json({ message: 'You are not login!' })
     }
     next();
 }
+
+module.exports = AuthLogin;
