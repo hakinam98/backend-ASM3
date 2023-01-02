9758d5b99fa61b407d515ee088002ac8e1ec99fe
dinhngocnam <namdnfx17199@funix.edu.vn>
2023-01-02 19:47:02 +0700

    CORS2

--- index.js
+++ index.js
@@ -98,12 +98,13 @@ app.use(async (req, res, next) => {
     }
 })
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+    next();
+})
+
 // app.use(function (req, res, next) {
 //     res.header('Access-Control-Allow-Credentials', true);
 //     res.header('Access-Control-Allow-Origin', '*');
